89e357e8638cd793f9d0b476cd552a599de45835
Alisson S. Zanetti <alissonzanetti2@gmail.com>
2023-06-08 00:46:20 -0300

    HTML and JS

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    // Aplicar máscaras aos campos
+    $('#cpf').mask('000.000.000-00');
+    $('#telefone').mask('(00) 0000-0000');
+    $('#cep').mask('00000-000');
+  });
